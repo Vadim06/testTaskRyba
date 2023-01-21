d87a9c2900844ef0889ebe52c2d4881cc5f3dbc5
Vadym <vadim.mikhaylets@gmail.com>
2023-01-21 13:01:19 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import icons from './assets/icons.json'
+import { useState, useEffect } from 'react';
+import Gallery from './components/gallery/Gallery';
+
+
+function App() {
+
+  return (
+    <>
+    <Gallery />
+    </>
+  );
+}
+
+export default App;
